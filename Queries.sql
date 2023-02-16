1ab8e62f8cc968e1038e23dbed8eea26bb881cf8
Isha <52040505+Parisha-Agrawal@users.noreply.github.com>
2023-02-16 21:59:39 +0530

    checking constraints query added

--- Queries.sql
+++ Queries.sql
@@ -64,5 +64,8 @@ SELECT * FROM `online retail store`.order o
 JOIN `online retail store`.billing b
 	ON b.orderID = o.orderID;
 
-
+-- 11 checking constraints
+SELECT CONSTRAINT_NAME, CONSTRAINT_TYPE
+FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS
+WHERE TABLE_NAME='product';
 
