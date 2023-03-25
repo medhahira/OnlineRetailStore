efa34afde3cabfcff9f39f9690f477ef88d21d26
Medha Hira <101352297+medhahira@users.noreply.github.com>
2023-03-25 16:23:20 +0530

    Update cli.py

--- cli.py
+++ cli.py
@@ -3,8 +3,6 @@ from datetime import datetime
 
 # datetime object containing current date and time
 now = datetime.now()
- 
-print("now =", now)
 
 # dd/mm/YY H:M:S
 dt= now.strftime("%Y-%m-%d %H:%M:%S")
