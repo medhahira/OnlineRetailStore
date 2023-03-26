cff04796e71258551c8a19ee5fb4bcf948577086
Medha Hira <101352297+medhahira@users.noreply.github.com>
2023-03-26 16:41:06 +0530

    Update triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -1,11 +1,7 @@
 CREATE TRIGGER remove_from_cart AFTER INSERT ON `Order`
 FOR EACH ROW
-BEGIN
    DELETE FROM Cart WHERE productID = NEW.productID;
-END;
 
 CREATE TRIGGER `update_funds_raised` AFTER INSERT ON `Billing`
 FOR EACH ROW
-BEGIN
   UPDATE `NGO` SET `funds_raised` = `funds_raised` + NEW.amount_donated WHERE `ngoID` = NEW.ngoID;
-END;
