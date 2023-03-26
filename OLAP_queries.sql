4c1ee69024db8a45076622075f0470973f55dd9b
Medha Hira <101352297+medhahira@users.noreply.github.com>
2023-03-26 15:37:10 +0530

    Update OLAP_queries.sql

--- OLAP_queries.sql
+++ OLAP_queries.sql
@@ -1,8 +1,6 @@
--- Suppose we want to analyze the total sales amount and quantity of products sold by category and sub-category for the last quarter
-
+-- View Quarterly Sales of the each Category
 SELECT
-    IFNULL(Category.category_name, 'Total') AS Category,
-    IFNULL(Product.storage_type, 'Total') AS Subcategory,
+    Category.category_name AS Category,
     SUM(`Order`.order_amount) AS Total_Sales_Amount,
     SUM(`Order`.quantity) AS Total_Quantity_Sold
 FROM
@@ -13,14 +11,12 @@ FROM
 WHERE
     `Order`.date_order_placed >= DATE_SUB(NOW(), INTERVAL 3 MONTH)
 GROUP BY
-    Category.category_name,
-    Product.storage_type WITH ROLLUP
+    Category.category_name WITH ROLLUP
 HAVING
-    Category IS NOT NULL OR Product.storage_type IS NULL
+    Category IS NOT NULL    
 
 
--- This query retrieves sales data for each category, year, and month, including the total sales, the number of unique customers, and the number of unique products sold in each group. It uses ROLLUP to calculate subtotals and grand totals for each level of grouping, and COALESCE to replace NULL values in the Category column with a string representing all categories. Finally, it orders the results by category, year, and month, with subtotals and grand totals included.
-
+--  View Curated Sales Data for Each Category
 SELECT
     COALESCE(c.category_name, 'All Categories') AS Category,
     YEAR(o.date_order_placed) AS Year,
@@ -33,4 +29,50 @@ FROM
     JOIN Product p ON o.productID = p.productID
     JOIN Category c ON p.categoryID = c.categoryID
 GROUP BY Category, Year, Month with ROLLUP
+HAVING
+    Month is NOT NULL
 ORDER BY Category, Year DESC, Month DESC;
+
+-- View Top 5 Customers(based on money spent)
+
+SELECT 
+CASE 
+WHEN GROUPING(username) = 1 THEN 'All Customers'
+ELSE username
+END AS Customer,
+SUM(order_amount) AS TotalAmount 
+FROM 
+`Order`
+GROUP BY username with
+ROLLUP
+HAVING 
+username IS NOT NULL 
+ORDER BY 
+TotalAmount DESC 
+LIMIT 
+5;
+
+-- Data of items in the Inventory for each storage type
+
+SELECT storage_type, SUM(quantity) AS amt 
+FROM Inventory 
+GROUP BY  storage_type with ROLLUP 
+HAVING storage_type is not null
+
+
+--GROUP BY CUBE
+SELECT 
+  `Categrory`.categoryID, 
+  Product.productID, 
+  o.username, 
+  SUM(o.order_amount) AS total_order_amount
+FROM `Order` o
+LEFT JOIN Product ON o.productID = Product.productID
+LEFT JOIN `Category` ON Product.categoryID = `Category`.categoryID
+GROUP BY CUBE(`Category`.category_name, Product.productID, o.username)
+
+
+-- grouping the products by their categoryID, return the sum of their prices for each category
+SELECT categoryID, SUM(price)
+FROM Product
+GROUP BY CUBE(categoryID);
