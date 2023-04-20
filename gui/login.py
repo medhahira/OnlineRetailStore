0509dc5b652190879c762efa6e5871be3c71ff61
Parisha <parishameerut@gmail.com>
2023-04-20 12:55:13 +0530

    gui updated

--- gui/login.py
+++ gui/login.py
@@ -1,26 +1,13 @@
 from tkinter import *
 from tkinter import messagebox
-# import sqlite3
 import mysql.connector
-import time
-from datetime import datetime
 from tabulate import tabulate
 from os import system
 
 
-try:
-        cnx = mysql.connector.connect(user='root', password='Pam@2302', 
-                              host='localhost', database='online retail store')
-        c = cnx.cursor()
-        c.execute("Select username,password from Admin")
-        un = []
-        pd = []
-        for i in c.fetchall():
-            un.append(i[0])
-            pd.append(i[1])
-        
-except Exception as ep:
-    messagebox.showerror('', ep)
+cnx = mysql.connector.connect(user='root', password='Pam@2302', 
+                        host='localhost', database='online retail store')
+c = cnx.cursor()
 
 ws = Tk()
 ws.title('Python Guides')
@@ -29,12 +16,13 @@ ws.config(bg="#447c84")
 ws.attributes('-fullscreen',True)
 
 
-# def createAccount():
-#     ws.destroy()
-#     import register
+def createAccount():
+    ws.destroy()
+    import register
 
 
 def submit():
+    t = login_type.get()
     u = uname.get()
     p = pwd.get()
     check_counter=0
@@ -47,14 +35,47 @@ def submit():
     else:
         check_counter += 1
     if check_counter == 2:
-        if ((u == un[0] and p == pd[0]) or (u == un[1] and p == pd[1])):
-            ws.destroy()
-            import app
-        
+        if t == "":
+            warn = "Login Type can't be empty."
         else:
-            messagebox.showerror('', 'invalid username or password')
+            if t == "admin":
+                query_auth_admin = """Select username,password from Admin"""
+                c.execute(query_auth_admin)
+                valid_admin = 0
+                for row in c.fetchall():
+                    if (u) == row[0] and (p) == row[1]:
+                        valid_admin = 1
+                        ws.destroy()
+                        import admin_app
+                if valid_admin == 0:
+                    messagebox.showerror('', f'Invalid Username or password. Please try again')
+            
+            elif t == "user":
+                query_auth_user = """Select username,password from Customer"""
+                c.execute(query_auth_user)
+                valid_user = 0
+                for row in c.fetchall():
+                    if (u) == row[0] and (p) == row[1]:
+                        valid_user = 1
+                        ws.destroy()
+                        import user_app
+                if valid_user == 0:
+                    messagebox.showerror('', f'Invalid Username or password. Please try again')
+
+
+            else:
+                messagebox.showerror('', "Can only login as admin and user") 
     else:
         messagebox.showerror('', warn)
+
+
+def logOut():
+   resp = messagebox.askquestion('', 'Are you sure?')
+   if resp == 'yes':
+        ws.destroy()
+        
+   else:
+        pass
        
 # frame
 frame = Frame(ws, padx=20, pady=20)
@@ -63,53 +84,32 @@ frame.pack(expand=True)
 
 
 # labels
-Label(
-    frame, 
-    text="Admin Login", 
-    font=("Times", "24", "bold") 
-    ).grid(row=0,  columnspan=3, pady=10) #..place(x=170, y=10)
+Label(frame, text="Login", font=("Times", "24", "bold")).grid(row=0,  columnspan=3, pady=10) #..place(x=170, y=10)
 
-Label(
-    frame, 
-    text='Enter Username', 
-    font=("Times", "14")
-    ).grid(row=1, column=1, pady=5) #.place(x=50, y=70)
+Label(frame, text='Login Type', font=("Times", "14")).grid(row=1, column=1, pady=5) #.place(x=50, y=70)
 
-Label(
-    frame, 
-    text='Enter Password', 
-    font=("Times", "14")
-    ).grid(row=2, column=1, pady=5) #.place(x=50, y=110)
+Label(frame, text='Enter Username', font=("Times", "14")).grid(row=2, column=1, pady=5) #.place(x=50, y=70)
+
+Label(frame, text='Enter Password', font=("Times", "14")).grid(row=3, column=1, pady=5) #.place(x=50, y=110)
 
 # Entry
+login_type = Entry(frame, width=20)
 uname = Entry(frame, width=20)
 pwd = Entry(frame, width=20, show="*")
 # uname.place(x=220, y=70)
 # pwd.place(x=220, y=110)
-uname.grid(row=1, column=2)
-pwd.grid(row=2, column=2)
+login_type.grid(row=1, column=2)
+uname.grid(row=2, column=2)
+pwd.grid(row=3, column=2)
 
 # button 
-# reg = Button(
-#     frame, 
-#     text="Create Account", 
-#     padx=20, pady=10, 
-#     relief=RAISED, 
-#     font=("Times", "14", "bold"), 
-#     command=createAccount
-#     )
+reg = Button(frame, text="Create Account", padx=20, pady=10, relief=RAISED, font=("Times", "14", "bold"), command=createAccount)
+reg.grid(row=4, column=1, pady=10)
 
-sub = Button(
-    frame, 
-    text="Login", 
-    padx=20, 
-    pady=10, 
-    relief=RAISED, 
-    font=("Times", "14", "bold"), 
-    command=submit
-    )
+sub = Button(frame, text="Login", padx=20, pady=10, relief=RAISED, font=("Times", "14", "bold"), command=submit)
+sub.grid(row=4, column=2, pady=10)
 
-# reg.grid(row=3, column=1, pady=10)
-sub.grid(row=3, column=2, pady=10)
+logout = Button(frame, text="Logout", padx=20, pady=10, relief=RAISED, font=("Times", "14", "bold"), command=logOut)
+logout.grid(row=4, column=3, pady=10)
 
 ws.mainloop()
---
+++ gui/register.py
@@ -0,0 +1,52 @@
+from tkinter import *
+from tkinter import messagebox
+
+ws = Tk()
+ws.title('Python Guides')
+ws.geometry('500x300')
+ws.config(bg="#4D4DFF")
+ws.attributes('-fullscreen',True)
+
+# functions
+def msg():
+    return messagebox.showinfo('', 'Life is short, \n do what you love')
+
+def logOut():
+   resp = messagebox.askquestion('', 'Are you sure?')
+   if resp == 'yes':
+        ws.destroy()
+        
+   else:
+        pass
+
+# frames
+frame = Frame(ws,padx=20,pady=20)
+frame.pack(expand=True)
+
+# image 
+# img = PhotoImage(file='img.png')
+
+# labelslo
+Label(
+     frame, 
+     text="Congratulations!",
+     font=("Times", "24", "bold")
+     ).grid(row=0, columnspan=3)
+
+Label(
+     frame, 
+     text='Your Account is Active', 
+     fg='green',
+     font=("Times", "14")
+     ).grid(row=1, columnspan=3)
+
+# imglbl = Label(frame, image=img)
+# imglbl.grid(row=2, column=1)
+
+# button 
+exp = Button(frame, text="open>>", padx=20, pady=10, relief=SOLID, font=("Times", "14", "bold"), command=msg)
+logout = Button(frame, text="Logout", padx=20, pady=10, relief=SOLID, font=("Times", "14", "bold"), command=logOut)
+exp.grid(row=2 , column=1)
+logout.grid(row=5, column=1)
+
+ws.mainloop()
